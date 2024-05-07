7251c7de55ebb65874e3e0fdfcf4894212102238
serhiidotsyk <dosyk11@gmail.com>
2024-05-07 20:48:16 +0300

    Improve transaction verification in Solana

--- feature_78.rs
+++ feature_78.rs
@@ -1,3 +1,5 @@
 // Feature update: Improving contract upgrade performance
 // Added support for scalability improvements
 // Code refactored for better efficiency
+
+// Improve data encryption in validator scripts.
